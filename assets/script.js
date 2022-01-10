887131cf8e4157e825e631f1bd4b79a880348736
Luketedwards <Luketedwards96@hotmail.com>
2022-01-10 20:19:27 +0000

    added modal

--- assets/script.js
+++ assets/script.js
@@ -50,3 +50,18 @@ for (let i = 0; i < suits.length; i++) {
   number++;
 }); 
 /*$('#guess').css('background-image', `url(/assets/images/${shuffledDeck[i]}.png`);*/
+
+let modalBtn = document.getElementById("modal-btn")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+}
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
