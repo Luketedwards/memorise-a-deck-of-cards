8e795312be0c8b4d01866db1d1db58bc8370fa50
Luketedwards <Luketedwards96@hotmail.com>
2022-01-10 01:49:30 +0000

    add card flip sound effects. Worked on card alignment

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
 
-var audio = new Audio('/assets/audio/Card-flip-sound-effect.mp3');
+var audioSuccess = new Audio('/assets/audio/Card-flip-sound-effect.mp3');
+var audioFail = new Audio('/assets/audio/denied.mp3');
 $('#cards-container div').click(function(){
+    if(this.classList.contains("card-is-flipped")){
+        audioFail.play()
+        alert('Already flipped!');
+        }else{
     console.log('you clicked' + " " + this.id);
-    $(this).addClass('card' + '-is-flipped' + " " + 'card-face--back');
-    audio.play();
+    $(this).addClass('card-is-flipped' + " " + 'card-face--back');
+    audioSuccess.play();}
+    
 });
