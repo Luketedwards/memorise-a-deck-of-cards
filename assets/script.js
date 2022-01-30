d620c33dc2c41eb6321fcb1c9c58c3d78db61b58
Luketedwards <Luketedwards96@hotmail.com>
2022-01-30 14:34:22 +0000

    Added high score button

--- assets/script.js
+++ assets/script.js
@@ -158,6 +158,25 @@ var cardClicked = $('.cards').click(function(event){
       })}
     })
 
+    function scoreList(event){
+    Swal.fire({
+      title: '<strong>HTML <u>example</u></strong>',
+      icon: 'info',
+      html:
+        'You can use <b>bold text</b>, ' +
+        '<a href="//sweetalert2.github.io">links</a> ' +
+        'and other HTML tags',
+      showCloseButton: true,
+      showCancelButton: true,
+      focusConfirm: false,
+      confirmButtonText:
+        '<i class="fa fa-thumbs-up"></i> Great!',
+      confirmButtonAriaLabel: 'Thumbs up, great!',
+      cancelButtonText:
+        '<i class="fa fa-thumbs-down"></i>',
+      cancelButtonAriaLabel: 'Thumbs down'
+    })
+  }
 /*$('#guess').css('background-image', `url(/assets/images/${shuffledDeck[i]}.png`);*/
 function correctCard(selectedCard){
   let j = 0;
