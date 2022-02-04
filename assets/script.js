3975794cced96dbe1eacc43b8c102fa9b59069c9
Luketedwards <Luketedwards96@hotmail.com>
2022-02-04 15:24:22 +0000

    file paths

--- assets/script.js
+++ assets/script.js
@@ -63,7 +63,7 @@ function cardsProgression(){
     
       var number = 0; 
      $("#next").click(function(){
-        $('#guess').css('background-image', `url(../images/Card-${shuffledDeck[parseInt(number)]}.png`);;
+        $('#guess').css('background-image', `url(assets/images/Card-${shuffledDeck[parseInt(number)]}.png`);;
         number++;
         }); 
         
@@ -96,11 +96,11 @@ $('#next').click(function(){
     
    
 var audioSuccess = new Audio('../audio/Card-flip-sound-effect.mp3');
-var audioFail = new Audio('..audio/denied.mp3');
+var audioFail = new Audio('assets/audio/denied.mp3');
 var audioGameOver = new Audio('audio/You-lose-game-over.mp3');
 var audioCompletedMemorising = new Audio('audio/completed-cards.mp3');
 var audioGameWon = new Audio('audio/Finale-sound-effect.mp3');
-var audioCardShuffle = new Audio('.audio/shuffling-cards.mp3');
+var audioCardShuffle = new Audio('/assets/audio/shuffling-cards.mp3');
 var audioTryAgain = new Audio('audio/try-again-new.mp3');
 var audioCorrectCards = new Audio('audio/Success-sound-effect.mp3')
 
